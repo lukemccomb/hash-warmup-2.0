b70be85de2f4f8e9a98c09bd8bb1b56742edd127
Luke McComb <lucasmccomb@gmail.com>
2014-07-09 09:46:38 -0600

    got first four tests to pass

--- lib/dog_hash.rb
+++ lib/dog_hash.rb
@@ -1,3 +1,5 @@
 DOG = {
-  :cattle_dog => {:preferences => [{:name => "Harleigh" }], :first_name => 'Harleigh', :color => 'brown', :names => ['Harleigh', 'brown']}
+  :cattle_dog => {:preferences => [{:name => "Harleigh"}], :first_name => 'Harleigh', :color => 'brown', :names => ['Harleigh', 'brown']},
+  "harleigh" => {:preferences => [{:favorite_toy => "frog"}, {:favorite_food => "steak"}]},
+  "trixie" => {:preferences => [{:favorite_toy => "frog"}, {:favorite_food => "steak"}]}
 }
